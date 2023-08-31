1da09f9fb07c674daf98d9ed74ce26baf095fa7e
GustaSilva5 <79280794+GustaSilva5@users.noreply.github.com>
2023-08-31 19:06:44 -0300

    Finalização de Projeto

--- calculadora.js
+++ calculadora.js
@@ -1,26 +1,24 @@
-function insert(num)
-{
-    var numero = document.getElementById('resultado').innerHTML;
+// Função de inserção de numero
+function insert (num){
+    var numero = document.getElementById('resultado').innerHTML
     document.getElementById('resultado').innerHTML = numero + num;
 }
-function limpar()
-{
+
+//função para limpar tela
+function clean (){
     document.getElementById('resultado').innerHTML = "";
 }
-function voltar()
-{
+
+//função para apagar ultimo numero 
+function back (){
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
-function calcular()
-{
+
+function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
+    if (resultado)
     {
         document.getElementById('resultado').innerHTML = eval(resultado);
-    }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "Nada..."
     }
 }
